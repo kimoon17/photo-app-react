e91a459e3af01a1689581fd499cd91b73aef13ea
greyewi1 <greyewi@gmail.com>
2021-04-28 19:32:37 +0500

    fix hw

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import ImagePost from "./Pages/ImagePost"
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import Burger from './Components/Burger'
+import {useState} from 'react'
 
 function App() {
+  const [user, setUser] = useState(window.localStorage.getItem('user') ? JSON.parse(window.localStorage.getItem('user')) : null)
+
   return (
     <div className="big-wrapper">
-      <Navbar/>
+      <Navbar user={user} setUser={setUser}/>
       <Burger />
       <Switch>
         <Route exact path="/" render={() => <Home/>}/>
-        <Route path="/login" render={() => <LogIn/>}/>
+        <Route path="/login" render={() => <LogIn setUser={setUser}/>}/>
         <Route path="/postimage" render={() => <PostImage/>}/>
         <Route path="/sign-up" render={() => <SignUp/>}/>
         <Route path="/imagepost" render={() => <ImagePost/>}/>
